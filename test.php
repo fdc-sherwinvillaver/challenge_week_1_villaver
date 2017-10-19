151508ac3ee176b1b01f786d2d8dbbd5c70c8c4a
Sherwin Villaver <fdc-sherwinvillaver@github.com>
2017-10-19 16:05:30 +0800

    test20

--- test.php
+++ test.php
@@ -3,6 +3,6 @@
 		<title>Test</title>
 	</head>
 	<body>
-		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta iure ipsa facere praesentium suscipit nisi rem at assumenda, aliquam ut sint. Consequatur hic, architecto! Voluptatum veniam, alias saepe temporibus dolores.
+		Are you sure?
 	</body>
 </html>
