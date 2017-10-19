f01d22d73668d69d930c6749d155d20e6f55a0f4
Sherwin Villaver <31604882+fdc-sherwinvillaver@users.noreply.github.com>
2017-10-19 14:09:00 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-define("TOKEN", "Sherwinvillaver");                                       // The secret token to add as a GitHub or GitLab secret, or otherwise as https://www.example.com/?token=secret-token
+define("TOKEN", "sherwinvillaver");                                       // The secret token to add as a GitHub or GitLab secret, or otherwise as https://www.example.com/?token=secret-token
 define("REMOTE_REPOSITORY", "git@github.com:fdc-sherwinvillaver/challenge_week_1_villaver.git"); // The SSH URL to your repository
 define("DIR", "/var/www/html/challenge_week_1_villaver");                          // The path to your repostiroy; this must begin with a forward slash (/)
 define("BRANCH", "refs/heads/master");                                 // The branch route
