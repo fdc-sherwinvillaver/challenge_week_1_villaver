7d236863ceecb71503f778aa813aef24ae2c7ca4
fdc-sherwinvillaver <fdc-sherwinvillaver@github.com>
2017-10-20 03:54:56 +0000

    test

--- github.php
+++ github.php
@@ -12,7 +12,7 @@ function pullRequest(){
   echo $output;
 }
   
-if($_SERVER['REQUEST_METHOD'] == "POST"){
+if($_SERVER['REQUEST_METHOD'] === "POST"){
     $data = json_decode(file_get_contents('php://input'), true);
     if($data['ref'] == 'refs/heads/GITHUB-WEBHOOK'){
         pullRequest();
